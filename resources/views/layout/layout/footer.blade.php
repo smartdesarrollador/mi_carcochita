3af269b0d0027aa726f7799db0ca55068283b8a8
smart <sistemadesignstyle@gmail.com>
2023-02-07 16:53:59 -0500

    estructura basica 1

--- resources/views/layout/layout/footer.blade.php
+++ resources/views/layout/layout/footer.blade.php
@@ -1,100 +1,100 @@
 <section id="footer">
     <div class="container">
-        <div class="row  text-sm-left text-md-left mt-5 pt-5">
-            <!-- inicio -->
-            <div class="col-md-3 mt-md-0">
-                <h5 class="text-white">Carbon y Brasas</h5>
-                <a href="index.php"><img src="{{ asset('assets/img/index/logo.png') }}" style="width:100px"></a>
-                <div class="text-center text-md-left text-white">
-                    <h5>Siguenos en:</h5>
-                    <ul class="list-unstyled list-inline">
-                        <li class="list-inline-item">
-                            <a href="https://www.facebook.com/polleriacarbonybrasas/" target="_blank"
-                                class="btn-floating btn-sm rgba-white-slight " style="font-size:30px">
-                                <i class="fab fa-facebook" style="color:#ffffff;font-size:2.5rem"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="https://instagram.com/" target="_blank"
-                                class="btn-floating btn-sm rgba-white-slight mx-1" style="font-size:30px">
-                                <i class="fab fa-instagram" style="color:#ffffff;font-size:2.5rem"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        <div class="row text-center text-center text-sm-left text-md-left">
 
-            <div class="col-md-3 mb-md-0 mb-3 text-white">
-                <h5 class="text-uppercase">Sobre Nosotros</h5>
-                <ul class="list-unstyled ">
-                    <!-- <li><a class="text-white text-decoration-none" href="#">Nuestra Historia</a></li> -->
-                    <li><a class="text-white text-decoration-none" href="{{ url('/carta') }}">Nuestra Carta</a></li>
-                    <li><a class="text-white text-decoration-none" href="{{ url('/contacto') }}">Contactanos</a></li>
-                    <li><a class="text-white text-decoration-none" href="{{ url('/') }}">Inicio</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3 mb-md-0 mb-3 text-white">
-                <h5 class="text-uppercase">Atención al Usuario</h5>
-                <ul class="list-unstyled">
-                    <li><a class="text-white text-decoration-none" href="{{ url('/libro-reclamaciones') }}">Libro de
-                            Reclamaciones</a></li>
-                    <li><a class="text-white text-decoration-none" href="{{ url('/terminos') }}">Términos y Condiciones
-                            Online</a></li>
-                    <li><a class="text-white text-decoration-none" href="{{ url('/politica-privacidad') }}">Políticas de
-                            Privacidad</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3 mb-md-0 mb-3 text-white">
-                <h5 class="text-uppercase">CONTACTO</h5>
-                <ul class="list-unstyled">
-                    <li><a class="text-white text-decoration-none" href="#!"><i class="fas fa-map-marker-alt mr-2"
-                                style="font-size:20px"></i>AV. CORONEL JOSE LEAL NRO. 649 URB. RISSO - LINCE</a></li>
-                    <li><a class="text-white text-decoration-none" href="#!"><i class="fas fa-envelope mr-2"
-                                style="font-size:20px"></i>reservaciones@carbonybrasas.pe</a></li>
-                    <!--  <li><a class="text-white text-decoration-none" href="#!"><i class="fas fa-phone mr-2" style="font-size:20px"></i>01-000000</a></li> -->
-                    <li><a class="text-white text-decoration-none"
-                            href="https://api.whatsapp.com/send?phone=51950293079&text=Hola%20quiero%20hacer%20un%20pedido"
-                            target="_blank"><i class="fab fa-whatsapp mr-2" style="font-size:25px"></i>950293079</a>
+            <div class="col-sm-12 col-sm-4 col-md-4 mt-3">
+                <h5 class="text-center">Redes Sociales</h5>
+                <ul class="list-unstyled text-center">
+                    <li class="mt-3"><a class="descripcion" target="_blank"
+                            href="https://www.facebook.com/Carcochita"><img class="" width="60px"
+                                src="{{ asset('assets/img/footer/icono-fb.png') }}" alt=""><br>Siguenos en
+                            Facebook</a>
                     </li>
+                    <li class="mt-3"><a class="descripcion" target="_blank"
+                            href="https://www.instagram.com/micarcochita/"><img class="" width="60px"
+                                src="{{ asset('assets/img/footer/icono-instagram.png') }}" alt=""><br>Siguenos
+                            en Instagram</a>
+                    </li>
+
+
                 </ul>
             </div>
-            <!-- fin -->
+            <div class="col-sm-12 col-sm-4 col-md-4 mt-3">
+                <h5 class="text-center">Contacto</h5>
+                <ul class="list-unstyled text-center">
+                    <li class="mt-3">
+                        <a class="descripcion" target="_blank" href="https://goo.gl/maps/iBBvecNiP26EJSRz9">
+                            <!-- <i class="fa fa-4x fa-map-marker" aria-hidden="true"></i> -->
+                            <img width="60px" src="{{ asset('assets/img/icons/ubicacion_mapa.png') }}" alt="">
+                            <br>Av. Julio César Tello 898, Lince, Lima - Perú
+                            <br>Av. Aviación 2739, San Borja, Lima - Perú
+                        </a>
+                    </li>
+                    <li class="mt-3">
+                        <a class="descripcion" target="_blank" href="tel:+017895285">
+                            <!-- <i class="fa fa-4x fa-phone" aria-hidden="true"></i> -->
+                            <img width="60px" src="{{ asset('assets/img/icons/icono_telefono.png') }}" alt="">
+                            <br>Delivery Lince: 017775912 / 923289681
+                        </a>
+                        <a class="descripcion" target="_blank" href="tel:+017858536">
+
+                            <br>Delivery San Borja: 017895285 / 922684862</a>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="col-sm-12 col-sm-4 col-md-4 mt-3">
+                <h5 class="text-center">Herramientas</h5>
+                <ul class="list-unstyled text-center">
+                    <li class="mt-3">
+                        <a class="descripcion" target="_blank" href="#">
+                            <img width="60px" src="{{ asset('assets/img/icons/libro.png') }}" alt=""><br>Libro
+                            de
+                            Reclamaciones
+                        </a>
+                    </li>
+                    <li class="mt-3">
+                        <a class="descripcion" target="_blank" href="#">
+                            <img width="60px" src="{{ asset('assets/img/icons/covid-19.png') }}" alt="">
+                            <br>
+                            Protocolos de Bioseguridad Covid-19
+                        </a>
+                    </li>
+
+
+                </ul>
+            </div>
 
 
         </div>
-        <div class="row">
-            <div class="d-none d-sm-none d-md-block col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-                <ul class=" list-unstyled list-inline social ">
-                    <li class="list-inline-item"><a href="https://www.facebook.com/polleriacarbonybrasas/"
-                            target="_blank"><i class="fab fa-facebook-square"></i></a></li>
-
-
-                    <li class="list-inline-item"><a href="mailto:reservaciones@carbonybrasas.pe" target="_blank"><i
-                                class="fa fa-envelope"></i></a></li>
-                </ul>
+        <div class="row mt-0 mt-xl-3 mt-lg-3">
+            <div class="col mt-2 text-center">
+                <h6 class="text-white descripcion">Desarrollado por Enfocus Soluciones</h6>
+                <img style="width: 150px" src="{{ asset('assets/img/footer/logoEnfocus.png') }}" alt="">
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-                <br>
-                <p class="h6"><a class="text-green ml-2" href="https://enfocussoluciones.com"
-                        target="_blank"><strong>&copy; Carbon y Brasas | Desarrollado por Enfocus
-                            Soluciones</strong></a>
+                <p>
+                    <!-- <a style="font-size: 18px; color: #FFFFFF;" href="cv.php">Bolsa de Trabajo </a>|<a style="font-size: 18px; color: #FFFFFF;" href="blog.php"> BLOG </a>|  --><a
+                        class="descripcion" href="#">
+                        Terminos y Condiciones</a>
                 </p>
+                <p class="h6 descripcion">&copy Todos los Derechos Reservados.<a class="text-green ml-2 descripcion"
+                        href="https://enfocussoluciones.com" target="_blank">Mi Carcochita - Enfocus</a></p>
             </div>
+            <hr>
         </div>
     </div>
 </section>
-
-
 
 <!-- Modal -->
 <div class="modal fade" data-backdrop="static" id="modalStoreSelector">
     <div class="modal-dialog " role="document">
         <div class="modal-content">
             <div class="modal-header text-center">
-                <h3 class="text-center m-auto">CAMBIAR DIRECCIÓN <img class="mb-2" src="assets/img/egypt.png"
-                        alt="" style="width: 35px"></h3>
+                <h3 class="text-center m-auto">CAMBIAR DIRECCIÓN <img class="mb-2"
+                        src="{{ asset('assets/img/egypt.png') }}" alt="" style="width: 35px"></h3>
             </div>
             <div class="modal-body">
                 <div class="row">
@@ -105,27 +105,6 @@
                         <div class="row justify-content-center">
                             <div class="col-12">
 
-
-
-
-                                <div class="row">
-                                    <div class="col">
-                                        <h5>Tu lugar de despacho es:</h5>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <ul>
-                                            <li>
-                                                <h5>
-                                                    <!-- Definir variable tiendaFooter -->
-                                                </h5>
-                                            </li>
-                                        </ul>
-
-                                    </div>
-                                </div>
 
 
 
@@ -168,20 +147,16 @@
                         </div>
 
                         <div class="row justify-content-center d-none" id="recojoInputContainer">
-                            <div class="text-center col-12">
-                                <!-- <h5>Selecciona un Local</h5>
+                            <div class="col-12">
+                                <h5>Selecciona un Local</h5>
                                 <div class="form-group">
                                     <select name="selectStoreSelector" id="selectStoreSelector"
-                                            class="form-control text-center">
+                                        class="form-control text-center">
                                         <option disabled selected>Selecciona un Local</option>
-                                        <option value="1">Julio Cesar Tello 872 - Lince</option>
-                                        <option value="2">Av. El Polo 121 - Surco</option>
+                                        <option value="1">Av. Julio César Tello 898, Lince</option>
+                                        <option value="2">Av. Aviación 2739, San Borja</option>
                                     </select>
-                                </div> -->
-
-                                <label class="" for="">Local de recojo: Brigadier Pumacahua 2321</label>
-                                <input type="hidden" value="1" name="selectStoreSelector"
-                                    id="selectStoreSelector">
+                                </div>
                             </div>
                         </div>
                         <div class="row">
--- resources/views/layout/layout/scripts_head.blade.php
+++ resources/views/layout/layout/scripts_head.blade.php
@@ -1,16 +1,6 @@
 <script src="https://apis.google.com/js/api:client.js"></script>
 <script src="{!! asset('assets/js/googleSignIn.js') !!}"></script>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129717590-12"></script>
-<script>
-      window.dataLayer = window.dataLayer || [];
 
-      function gtag() {
-            dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', 'UA-129717590-12');
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArTyoCYqHm0479cO0A1WNMv3w1X0uLjQo&libraries=places">
 </script>
-
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcvTQDOpZWw0rMVg2YXBB9joWgZvLuY0U&libraries=places"></script>
